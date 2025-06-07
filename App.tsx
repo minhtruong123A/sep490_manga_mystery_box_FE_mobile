b0176748ee6ff73296fe1248abe4073664f17541
LeBMinh <155410101+LeBMinh@users.noreply.github.com>
2025-06-08 01:01:29 +0700

    routeOnConstruction

--- App.tsx
+++ App.tsx
@@ -5,12 +5,45 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import icons
+
 // import screen
-
-
+import Shop from './src/screens/Shop';
+import Auction from './src/screens/Auction';
+import Profile from './src/screens/Profile';
+import BoxShop from './src/screens/BoxShop';
+import ProductShop from './src/screens/ProductShop';
+import BoxDetail from './src/screens/BoxDetail';
+import ProductDetail from './src/screens/ProductDetail';
+import Favorite from './src/screens/Favorite';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+// Stack Navigator for Shop (holds top Tabs) > BoxShop/ProductShop > Detail page 
+function BoxShopStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Mystery Box" component={BoxShop}
+        options={{ headerShown: false }} />
+      <Stack.Screen name="Box Detail" component={BoxDetail}
+        options={{ headerShown: true }} />
+    </Stack.Navigator>
+  );
+}
+
+function ProductShopStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Collection Store" component={ProductShop}
+        options={{ headerShown: false }} />
+      <Stack.Screen name="Collection Detail" component={ProductDetail}
+        options={{ headerShown: true }} />
+    </Stack.Navigator>
+  );
+}
+
+//
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -32,8 +65,34 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <Tab.Navigator
+          initialRouteName='Home Page'
+          screenOptions={{
+            tabBarActiveTintColor: '#F4B400',  // Custom active color
+            tabBarInactiveTintColor: 'gray', // Custom inactive color
+          }}
+        >
+          <Tab.Screen
+            name="Home Page"
+            component={HomeStack}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color }) => (
+                <Entypo name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Saved Toys"
+            component={SaveStack}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name="bookmark" size={24} color={color} />
+              ),
+            }} />
+        </Tab.Navigator> */}
     </NavigationContainer>
   );
 }
---
+++ src/screens/BoxDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BoxDetail() {
+  return (
+    <div>BoxDetail</div>
+  )
+}
